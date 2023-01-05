7d4c41916011bc66d87e35390293d3842a723b2e
alokaman <122042086+alokaman@users.noreply.github.com>
2023-01-05 22:17:15 +0530

    Add files via upload

---
+++ INT CLASS.py
@@ -0,0 +1,47 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2+4
+6
+>>> 2*4
+8
+>>> (6+5)*2
+22
+>>> a=4
+>>> a
+4
+>>> b=62+4b
+SyntaxError: invalid decimal literal
+>>> b=6
+>>> b
+6
+>>> c=a+b
+>>> c
+10
+>>> print(c)
+10
+>>> print("the value addition of two numbers)
+...       
+SyntaxError: unterminated string literal (detected at line 1)
+>>> 
+
+... 0
+>>> print("the value the value of two numbers")
+...       
+the value the value of two numbers
+>>> 6+4
+...       
+10
+>>> print("the value the sum of two numbers")
+...       
+the value the sum of two numbers
+>>> a=int(input("enter the value of first element"))
+...       
+enter the value of first element5
+>>> b=int(input("enter the value of second element"))
+...       
+enter the value of second element6
+>>> c=b-a
+...       
+>>> print("the result of subtraction is:",c)
+...       
+the result of subtraction is: 1
